09ae73be85b9329c1577c48f934a783d7b968d7b
guanlongtianzi <mr.zhaoqi@foxmail.com>
2015-04-05 12:21:50 +0800

    Create global.R

---
+++ global.R
@@ -0,0 +1,39 @@
+if(!require(shiny)){
+  install.packages(pkgs = 'shiny',quiet = TRUE)
+  require(shiny)
+}
+
+if(!require(DMwR)){
+  install.packages(pkgs = 'DMwR',quiet = TRUE)
+  require(DMwR)
+}
+
+if(!require(ggplot2)){
+  install.packages(pkgs = 'ggplot2',quiet = TRUE)
+  require(ggplot2)
+}
+
+if(!require(ROCR)){
+  install.packages(pkgs = 'ROCR',quiet = TRUE)
+  require(ROCR)
+}
+
+if(!require(shinyAce)){
+  install.packages(pkgs = 'shinyAce',quiet = TRUE)
+  require(shinyAce)
+}
+
+if(!require(rmarkdown)){
+  install.packages(pkgs = 'rmarkdown',quiet = TRUE)
+  require(rmarkdown)
+}
+
+if(!require(gridExtra)){
+  install.packages(pkgs = 'gridExtra',quiet = TRUE)
+  require(gridExtra)
+}
+
+if(!require(e1071)){
+  install.packages(pkgs = 'e1071',quiet = TRUE)
+  require(e1071)
+}
